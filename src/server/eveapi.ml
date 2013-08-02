ae609e4a09e7d79fe08fc444949444f39194f911
Drup <drupyog@zoho.com>
2013-08-02 21:52:51 +0200

    Use Clist and new apis.

--- src/server/eveapi.ml
+++ src/server/eveapi.ml
@@ -11,6 +11,8 @@ module Response = struct
     | Tags : (string * string) list extract
     | Rowset : (string * string) list list extract
     | Rowsets : (string * string) list list list extract
+    | Rowset2 : (string * string) list list list extract
+    | Raw : Simplexmlparser.xml list extract
 
   type 'a t = {
     version : string ;
@@ -30,6 +32,11 @@ module Response = struct
   let parse_rowset = function
     | Element ("rowset", [("name", name); ("key", key); ("columns", col)], rows)
       -> List.map parse_row rows
+    | _ -> raise (Wrong "rowset")
+
+  let parse_rowset2 = function
+    | Element ("rowset", [("name", name); ("key", key); ("columns", col)], rows)
+      -> List.map parse_rowset rows
     | _ -> raise (Wrong "rowset")
 
   let parse_tag = function
@@ -57,18 +64,28 @@ module Response = struct
 
   let extract_rowsets r = map (List.map parse_rowset) (unwrap r)
 
+  let extract_rowset2 r  =
+    let f = function
+      | [ l ] -> parse_rowset2 l
+      | _ -> raise (Wrong "rowset2")
+    in map f (unwrap r)
+
   let extract (type a) (ex : a extract) xml : a t = match ex with
     | Tags -> extract_tags xml
     | Rowset -> extract_rowset xml
+    | Rowset2 -> extract_rowset2 xml
     | Rowsets -> extract_rowsets xml
+    | Raw -> unwrap xml
 
 end
 
-(** Conveniance stuff *)
+(** Convenience stuff *)
 
 let aoc = List.assoc
 let ios = int_of_string
+let soi = string_of_int
 let fos = float_of_string
+let sof = string_of_float
 
 module OS = Ocsigen_stream
 
@@ -104,7 +121,7 @@ let apikey ~keyId ~vCode : apikey =
     method keyId = keyId
     method vCode = vCode
   end
-let encode_apikey k =
+let encode_apikey (k : apikey) =
   [ ("keyID", string_of_int (k#keyId)) ; ("vCode", k#vCode) ]
 
 type charkey = < keyId : int ; vCode : string ; characterID : int >
@@ -114,41 +131,23 @@ let apikey ~keyId ~vCode ~charId : charkey =
     method vCode = vCode
     method characterID = charId
   end
-let encode_charkey k =
-  ("characterID", string_of_int (k#characterID)) :: encode_apikey k
+let encode_charkey (k : charkey) =
+  ("characterID", string_of_int (k#characterID)) :: encode_apikey (k :> apikey)
 
 type enc_param = (string * string) list
 
-type (_,_) param =
-  | NoParam : ('out,'out) param
-  | Param : ('a -> enc_param) -> ('out,'a -> 'out) param
-
-type (_,_) auth =
-  | NoAuth : ('a, 'a) auth
-  | ApiKey : ('a, apikey -> 'a) auth
-  | CharKey : ('a, charkey -> 'a) auth
-
-let api_call (type tf) (type tp) (type tout)
-    (p : (tout, tp) param)
-    (k : (tp, tf) auth)
-    (cont : enc_param -> tout) : tf =
-  match k, p with
-    | NoAuth, NoParam -> cont []
-    | ApiKey, NoParam -> fun a -> cont (encode_apikey a)
-    | CharKey, NoParam -> fun a -> cont (encode_charkey a)
-    | NoAuth, Param f -> fun b -> cont (f b)
-    | ApiKey, Param f -> fun a b -> cont (encode_apikey a @ f b)
-    | CharKey, Param f -> fun a b -> cont (encode_charkey a @ f b)
-
-type ('extract, 'funtype , 'param, 'out) internal_api = {
+type ('extract, 'a , 'b, 'c) internal_api = {
   uri : string ;
   cache : cache ;
   result : 'extract Response.extract ;
-  auth : ('param, 'funtype) auth ;
-  param : ('out Lwt.t, 'param) param ;
-  decode : 'extract -> 'out
+  auth : (enc_param, 'b, 'c) Clist.t ;
+  param : (enc_param, 'a Lwt.t, 'b) Clist.t ;
+  decode : 'extract -> 'a
 }
-type _ api = Api : (_, 'a, _ , _) internal_api -> 'a api
+type _ api = Api : (_, _, _, 'a) internal_api -> 'a api
+
+let get_cache (Api x) = x.cache
+let get_uri (Api x) = x.uri
 
 let apply_api prefix (Api endpoint) =
   let cont args =
@@ -156,12 +155,36 @@ let apply_api prefix (Api endpoint) =
     let data = Response.( (extract endpoint.result response).data ) in
     Lwt.return (endpoint.decode data)
   in
-  api_call endpoint.param endpoint.auth cont
+  Clist.(glue list_mono cont (append endpoint.auth endpoint.param))
 
-(** Spectific APIs *)
+(** {2 Spectific APIs} *)
+
+(** {5 Account} *)
+
+let accountStatus =
+  Api {
+    uri = "/account/AccountStatus.xml.aspx" ;
+    cache = Short ;
+    auth = Clist.singfun encode_apikey ;
+    param = Clist.Nil ;
+    result = Response.Tags;
+    decode = fun i -> i ;
+  }
+let get_accountStatus = apply_api tq accountStatus
+
+let apiKeyInfo =
+  Api {
+    uri = "/account/APIKeyInfo.xml.aspx" ;
+    cache = Short ;
+    auth = Clist.singfun encode_apikey ;
+    param = Clist.Nil ;
+    result = Response.Rowset;
+    decode = fun i -> i ;
+  }
+let get_apiKeyInfo = apply_api tq apiKeyInfo
 
 let characters =
-  let dec = function
+  let dec x = match x with
     | [ "name",name ; "characterID",characterID ; "corporationName",corpID ;
         "corporationName", corpName ] ->
         object
@@ -175,35 +198,113 @@ let characters =
   Api {
     uri = "/account/Characters.xml.aspx" ;
     cache = Short ;
-    auth = ApiKey ;
-    param = NoParam ;
+    auth = Clist.singfun encode_apikey ;
+    param = Clist.Nil ;
     result = Response.Rowset ;
     decode = List.map dec ;
   }
 let get_characters = apply_api tq characters
+
+(** {5 Character } *)
+
+let accountBalance =
+  Api {
+    uri = "/char/AccountBalance.xml.aspx" ;
+    cache = Short ;
+    auth = Clist.singfun encode_charkey ;
+    param = Clist.Nil ;
+    result = Response.Rowset ;
+    decode = fun i -> i ;
+  }
+let get_accountBalance = apply_api tq accountBalance
+
+let assetList =
+  Api {
+    uri = "/char/AssetList.xml.aspx" ;
+    cache = Long ;
+    auth = Clist.singfun encode_charkey ;
+    param = Clist.Nil ;
+    result = Response.Rowset2 ;
+    decode = fun i -> i ;
+  }
+let get_assetList = apply_api tq assetList
+
+(* http://wiki.eve-id.net/APIv2_Char_CalendarEventAttendees_XML *)
+(* TODO multiple event virgule separated *)
+let calendarEventAttendees =
+  Api {
+    uri = "/char/CalendarEventAttendees.xml.aspx" ;
+    cache = MShort ;
+    auth = Clist.singfun encode_charkey ;
+    param = Clist.singfun (fun x -> [ "eventIDs", soi x ]) ;
+    result = Response.Rowset ;
+    decode = fun i -> i ;
+  }
+let get_calendarEventAttendees = apply_api tq calendarEventAttendees
+
+(* http://wiki.eve-id.net/APIv2_Char_CharacterSheet_XML *)
+let characterSheet =
+  Api {
+    uri = "/char/CharacterSheet.xml.aspx" ;
+    cache = MShort ;
+    auth = Clist.singfun encode_charkey ;
+    param = Clist.Nil ;
+    result = Response.Raw ;
+    decode = fun i -> i ;
+  }
+let get_characterSheet = apply_api tq characterSheet
+
+(* http://wiki.eve-id.net/APIv2_Char_JournalEntries_XML *)
+let walletJournal =
+  let enc_fromID = function | Some i -> [ "fromID", soi i ] | None -> []
+  and enc_rowCount = function | Some i -> [ "rowCount", soi i ] | None -> []
+  in
+  Api {
+    uri = "/char/WalletJournal.xml.aspx" ;
+    cache = MShort ;
+    auth = Clist.singfun encode_charkey ;
+    param = Clist.(Param enc_fromID @+ Param enc_rowCount @+ Nil) ;
+    result = Response.Rowset ;
+    decode = fun i -> i ;
+  }
+let get_walletJournal ?fromID ?rowCount ~key =
+  apply_api tq walletJournal key fromID rowCount
+
+(* http://wiki.eve-id.net/APIv2_Char_MarketTransactions_XML *)
+let walletTransactions =
+  let enc_fromID = function | Some i -> [ "fromID", soi i ] | None -> []
+  and enc_rowCount = function | Some i -> [ "rowCount", soi i ] | None -> []
+  in
+  Api {
+    uri = "/char/WalletTransactions.xml.aspx" ;
+    cache = MShort ;
+    auth = Clist.singfun encode_charkey ;
+    param = Clist.(Param enc_fromID @+ Param enc_rowCount @+ Nil) ;
+    result = Response.Rowset ;
+    decode = fun i -> i ;
+  }
+let get_walletTransactions ?fromID ?rowCount ~key =
+  apply_api tq walletTransactions key fromID rowCount
+
+(** {5 Corporation } *)
+
+(** {5 Eve } *)
 
 let characterInfo =
   let enc cID = [ ("characterID", cID) ] in
   Api {
     uri = "/eve/CharacterInfo.xml.aspx" ;
     cache = Short ;
-    auth = NoAuth ;
-    param = Param enc ;
+    auth = Clist.Nil ;
+    param = Clist.singfun enc ;
     result = Response.Tags ;
     decode = fun i -> i ;
   }
 let get_characterInfo = apply_api tq characterInfo
 
-(* type (_,_) compo_list = *)
-(*   | Nil : ('a,'a) compo_list *)
-(*   | NoFun : ('a, 'b) compo_list -> ('a, 'b) compo_list *)
-(*   | Fun : ('b -> 'c) * ('b, 'a) compo_list -> ('c, 'a) compo_list *)
-(*   | FunParam : ('b -> 'p -> 'c) * ('b, 'a) compo_list -> ('p -> 'c, 'a) compo_list *)
 
-(* let rec get_fun : type t1 t2 . (t1, t2) compo_list -> t2 -> t1 = function *)
-(*   | Nil -> (fun x -> x) *)
-(*   | NoFun l -> get_fun l *)
-(*   | Fun (f,l) -> let fl = get_fun l in fun x -> (f (fl x)) *)
-(*   | FunParam (f,l) -> let fl = get_fun l in fun x -> (f (fl x)) *)
+(** {5 Map } *)
 
-(* let l = Fun (float ,FunParam ((+), Nil)) *)
+(** {5 Server } *)
+
+(** {5 API } *)
